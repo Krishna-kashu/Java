9d41d655b314205b6572727db8e2686bd35ea38c
Krishna-kashu <krishnavenin8192@gmail.com>
2025-03-21 15:29:24 +0530

    pushing all the code to git

---
+++ Boy.java
@@ -0,0 +1,7 @@
+class Boy{
+	public static void main(String[] args){
+	for(int i=16;i<=24;i++){
+			System.out.println("System.out.println(\""+i+"th brand is : \" + brands["+(i-1)+"]);");
+		}
+	}
+}
