55b4cde9bc2c64f0be8f1aa1502763138a310738
Krishna-kashu <krishnavenin8192@gmail.com>
2025-03-23 12:49:31 +0530

    pushing all the code to git

---
+++ Bomb.java
@@ -0,0 +1,5 @@
+class Bomb{
+    static void details(String type, double weight, String material, String explosiveType, double blastRadius, double price, String fuseType, double detonationTime, String safetyMechanism, String manufacturer) {
+        System.out.println("Bomb Details - Type: " + type + ", Weight: " + weight + ", Material: " + material + ", Explosive Type: " + explosiveType);
+    }
+}
---
+++ BombRunner.java
@@ -0,0 +1,13 @@
+class BombRunner{
+	public static void main(String[] args){
+		for (int i = 0; i < 5; i++) {
+            Bomb.details("C4", 5, "Plastic", "High Explosive", 50, 1000, "Remote", 30, "Safety Pin", "ABC Munitions");
+            Bomb.details("C5", 5, "Plastic", "Low Explosive", 60, 2000, "Remote", 40, "Safety Pin", "ABB ");
+			Bomb.details("C6", 5, "Plastic", "High Explosive", 70, 3000, "Mannual", 60, "Pin", "ABY Munitions");
+			Bomb.details("C7", 5, "Plastic", "Low Explosive", 80, 4000, "mannual", 70, "Safety Pin", "FBC ");
+			Bomb.details("C8", 5, "Plastic", "High Explosive", 70, 5000, "Remote", 60, "Pin", "HBC Munitions");
+			Bomb.details("C9", 5, "Plastic", "Low Explosive", 50, 6000, "Remote", 30, "Safety Pin", "YBC Munitions");
+	}
+	}
+	
+}
