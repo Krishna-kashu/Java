1b17bc9b6be9724f641508c0cabcedf0fac1ec97
Krishna-kashu <krishnavenin8192@gmail.com>
2025-03-26 23:09:52 +0530

    java files

---
+++ Bullet.java
@@ -0,0 +1,5 @@
+class Bullet{
+    static void displayInfo(String type, double caliber, double weight, String material, double price) {
+        System.out.println("Bullet Info - Type: " + type + ", Caliber: " + caliber + ", Weight: " + weight + ", Material: " + material + ", Price: " + price);
+    }
+}
---
+++ BulletRunner.java
@@ -0,0 +1,11 @@
+class BulletRunner{
+	public static void main(String[] args){
+		Bullet.displayInfo("FMJ", 9.0, 124, "Lead", 2);
+		Bullet.displayInfo("\nMMM", 5.0, 144, "Lead", 3);
+		Bullet.displayInfo("\nFJJ", 6.0, 123, "Lead", 1);
+		Bullet.displayInfo("\nYTR", 7.0, 126, "Lead", 4);
+		Bullet.displayInfo("\nST", 8.0, 124, "Lead", 3);
+		Bullet.displayInfo("\nFMT", 7.0, 122, "Lead", 2);
+	}
+	
+}
